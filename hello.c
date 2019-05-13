2fc85097b15e5abf0b36e051cef618c8641e822c
Trinbow <50457375+Trinbow@users.noreply.github.com>
2019-05-13 14:38:35 +0200

    esercizio array of caracters

--- hello.c
+++ hello.c
@@ -1,13 +1,52 @@
 #include<stdio.h>
+#include<stdlib.h>
+#define N 10
+void stampacaracter(char c[N]){
+     int i;
+     for(i=0;i<N;i++){
+
+       printf("%3c",c[i]);
+  }
+   printf("\n\n");
+}
+void getcaracter(char c[N]){
+     int i;
+     for(i=0;i<N;i++){
+
+       printf("\ncarattere %2d: ",i+1); 
+       scanf(" %c",&c[i]);
+  }
+
+}
+void   print_senza(char c[N]){
 
 
-int main(){
+    int i ,j ; 
+
+     for(i=0;i<N;i++){
+         for(j=0;j<i;j++){
+             
+                   if(c[i]==c[j]){break;}          
+         }
 
 
-printf("fuck you men lekher \n\n fucking shit man");
-printf("fuck you men lekher \n\n thank you brow");
+         if(i==j){ printf("%3c",c[i]);}
+     }
 
 
 
 
+}
+int main (){
+  char c[N];
+  int i,j;
+  getcaracter(c);
+  stampacaracter(c);
+  print_senza(c);
+  printf("\n");
+ 
+ 
+
+
+return 0;
 }
