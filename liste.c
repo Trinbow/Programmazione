8d45da60493554498ceb38baec4747b6a2d37041
Xitan <yardiabdelkader@gmail.com>
2019-05-21 06:31:36 +0200

    edit liste.c zedt deletlis and delete node

--- liste.c
+++ liste.c
@@ -97,7 +97,7 @@ node *creat_list(node *temp,int val){
 }
 
 void stampa(node *temp){
-
+    //  if(temp==NULL){printf("\nla lista e' vuota");}
       if(temp !=NULL){printf("%3d",temp->data);stampa(temp->next);}
       else{printf("\n");}
 }
@@ -143,7 +143,57 @@ int  countm(node* temp)
       count=count+countm(temp->next);
 return count;
 }
+/*void delete(node **temp){
+     node *t1=*temp,*next;
+     while(t1!=NULL){
+         next=t1->next;
+         free(t1);
+         t1=next;
 
+
+     }
+
+
+      *temp=NULL;
+}*/
+node *destroy(node *temp){
+     node *current=temp,*next;
+     while(current !=NULL){
+         next=current->next;
+         free(current);
+         current=next;
+     }
+
+     temp=NULL;
+return temp;
+}
+
+int lenght(node *temp){
+     int count =0;
+     if(temp==NULL){return 0;}
+     if(temp !=NULL){count=1+lenght(temp->next);}
+     /*while(temp!=NULL){
+        count++;
+        temp=temp->next;
+    }*/
+return count;
+}
+node * delete_node(node *temp){
+     node*p=temp,*q;
+     int loc,i=1;
+     printf("\nenter the location:");
+     scanf("%d",&loc);
+     if(loc >lenght(root)){printf("\nINvalid location\n");}
+     if(loc==1){temp=p->next;p->next=NULL;free(p);}
+     else{
+         while(i<loc-1){p=p->next;i++;}
+         q=p->next;
+         p->next=q->next;
+         q->next=NULL;
+         free(q);
+     }
+  return temp;
+}
 int main(){
 
       int val;
@@ -152,13 +202,18 @@ int main(){
       do{
           scanf("%d",&val);
           if(val!=-1)
-            list=creat_list(list,val);
+          list=creat_list(list,val);
     	}while(val !=-1);
 
      stampa(root);
      list=reverse(root);
      stampa(root);
      printf("\nci sono %d multipli di 3.\n",countm(root));
+     root=delete_node(root);
+     stampa(root);
+     root=destroy(root);
+     //delete(&root);
+     stampa(root);
 
 return 0;
 
