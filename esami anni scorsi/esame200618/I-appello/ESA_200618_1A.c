0e8f594ab79401fcf41156ad45356cbb4a144df2
Xitan <yardiabdelkader@gmail.com>
2019-06-11 16:24:46 +0200

    soluzione del esercizio 1 A ricorsiva

---
+++ esami anni scorsi/esame200618/I-appello/ESA_200618_1A.c
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+//prottotipi delle funzioni 
+struct node_t* crea_lista(char *s);
+void stampa_lista(struct node_t*temp);
+void  triplica_vocali(struct node_t*temp);
+struct node_t* distruggi(struct node_t*temp);
+bool isvocal(char c);
+struct node_t * crea_nodo(char v, struct node_t * next);
+
+
+
+//crea il nodo 
+struct node_t{
+	char value;
+	struct node_t * next;
+};
+struct node_t* root=NULL;//dechiaro un puntatore che punata ala testa della lista come un variabile globale 
+
+//la funzione main 
+int main(){
+	char s[] = "la mia stringa";
+	root = crea_lista(s);
+	stampa_lista(root);
+	triplica_vocali(root);
+	stampa_lista(root);
+	root=distruggi(root);
+        stampa_lista(root);
+	return 0;
+}
+
+
+//creo una lista da una stringa 
+struct node_t* crea_lista(char *s){
+
+     struct node_t *temp;
+     int size=strlen(s);printf("\nsize=%d\n",size);    
+     int i;
+     for(i=0;i<size;i++){
+            temp=(struct node_t *)malloc(sizeof(struct node_t));
+            temp->value=s[i];
+            temp->next=NULL;
+            if(root==NULL){root=temp;}
+            else{
+                struct node_t *p=root;
+                while(p->next !=NULL){p=p->next;}
+                    p->next=temp;
+                }
+
+     }
+      
+
+
+return root;
+
+}
+//stampo la lista
+void stampa_lista(struct node_t*temp){
+       if(temp==NULL){
+           printf("\nla lista e vuota .\n");
+       }
+       else{
+           while(temp !=NULL){
+                printf("%2c",temp->value);
+                temp=temp->next;
+            }
+            printf("\n\n");
+      }
+}
+//la funzione che triplica i vocali in modo ricorsivo 
+void  triplica_vocali(struct node_t*temp){
+    
+    if(temp !=NULL){
+         if(isvocal(temp->value)){
+              temp->next = crea_nodo(temp->value, crea_nodo(temp->value, temp->next));
+	      triplica_vocali(temp->next->next->next);
+              
+         }
+         else{
+              triplica_vocali(temp->next);
+         }         
+
+    }
+    else{printf("\n\n");}
+}
+
+
+//verifica se il valore e un vocali o no 
+bool isvocal(char c){
+
+return (c=='a' ||c=='u' ||c=='e' ||c=='i' ||c=='o' );
+}
+
+struct node_t * crea_nodo(char v, struct node_t * next){
+	struct node_t * n = (struct node_t *) malloc(sizeof(struct node_t));
+	n->value = v;
+	n->next = next;
+	return n;
+}
+//destroy la lista 
+
+struct node_t *distruggi(struct node_t*temp){
+     if(temp==NULL){
+        return NULL;
+     }
+     else{
+         struct node_t*p=temp->next;
+         free(temp);
+         temp=p;
+        return distruggi(p);
+
+     }
+
+}
+
+
+
